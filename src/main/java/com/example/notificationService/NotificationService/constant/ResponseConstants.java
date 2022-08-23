397e55676688289e0f78d984e521b03ceffae0bc
Yash Porwal <yashporwal@ip-192-168-235-248.ap-southeast-1.compute.internal>
2022-08-23 18:25:26 +0530

    correction in ResponseConstants

--- src/main/java/com/example/notificationService/NotificationService/constant/ResponseConstants.java
+++ src/main/java/com/example/notificationService/NotificationService/constant/ResponseConstants.java
@@ -3,6 +3,6 @@ package com.example.notificationService.NotificationService.constant;
 public class ResponseConstants {
 
     public static final String SUCCESSFULLY_BLACKLISTED = "Successfully blacklisted";
-    public static final String SUCCESSFULLY_WHITELISTED = "Successfully blacklisted";
+    public static final String SUCCESSFULLY_WHITELISTED = "Successfully whitelisted";
     public static final String SUCCESSFULLY_SENT = "Successfully sent";
 }
