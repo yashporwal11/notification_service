cfe8b4ab750f7f20beb397021b73ecea291d1092
Yash Porwal <yashporwal@ip-192-168-235-248.ap-southeast-1.compute.internal>
2022-08-24 12:58:43 +0530

    basic authentication

---
+++ src/main/java/com/example/notificationService/NotificationService/config/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.notificationService.NotificationService.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${auth.user}")
+    private String username;
+    @Value("${auth.password}")
+    private String password;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic();
+
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authentication)
+            throws Exception
+    {
+        authentication.inMemoryAuthentication()
+                .withUser(username)
+                .password(passwordEncoder().encode(password))
+                .authorities("admin");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
